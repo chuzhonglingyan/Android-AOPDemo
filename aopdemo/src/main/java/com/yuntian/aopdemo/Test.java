40038a20101fbc0f4fe29153bb8b7cd928331a4a
yuntian <944610721@qq.com>
2018-11-11 19:50:36 +0800

    签名研究

---
+++ aopdemo/src/main/java/com/yuntian/aopdemo/Test.java
@@ -0,0 +1,14 @@
+package com.yuntian.aopdemo;
+
+public class Test {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
