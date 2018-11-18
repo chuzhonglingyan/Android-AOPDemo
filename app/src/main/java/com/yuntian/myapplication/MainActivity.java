3eefb001588ce8ae4ed8af874e494f1dd59e9cff
yuntian <944610721@qq.com>
2018-11-18 13:59:12 +0800

    打包测试

--- app/src/main/java/com/yuntian/myapplication/MainActivity.java
+++ app/src/main/java/com/yuntian/myapplication/MainActivity.java
@@ -1,11 +1,8 @@
 package com.yuntian.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
-
-import com.yuntian.aoplib.annotation.DebugTrace;
-import com.yuntian.aoplib.annotation.LogTime;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,26 +16,7 @@ public class MainActivity extends AppCompatActivity {
         tv_aspect=findViewById(R.id.tv_aspect);
         tv_javassist=findViewById(R.id.tv_javassist);
 
-        tv_aspect.setOnClickListener((v)->{ testAnnotatedMethod();});
-        tv_javassist.setOnClickListener((v)->{ testJavassistMethod();});
-
     }
 
-    @DebugTrace
-    private void testAnnotatedMethod() {
-        try {
-            Thread.sleep(10);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
 
-    @LogTime
-    private void testJavassistMethod() {
-        try {
-            Thread.sleep(10);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
 }
